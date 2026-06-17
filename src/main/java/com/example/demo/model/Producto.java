e0ba485c6963b96ecdde9d6699162e083fa564bf
Antonella Lezcano <lezcanoantonella85@gmail.com>
2026-06-17 18:59:30 -0300

    feat: proyecto base y configuracion bd

---
+++ src/main/java/com/example/demo/model/Producto.java
@@ -0,0 +1,84 @@
+package com.example.demo.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity 
+@Table(name = "productos")
+public class Producto {
+
+	//// id definido como autoincremental
+	@Id 
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@Column(nullable = false)
+	private String nombre;
+	
+	@Column
+	private String descripcion;
+	
+	@Column(nullable = false)
+	private Double precio;
+	
+	@Column
+	private Integer stock;
+	
+	@Column
+	private Boolean activo;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getDescripcion() {
+		return descripcion;
+	}
+
+	public void setDescripcion(String descripcion) {
+		this.descripcion = descripcion;
+	}
+
+	public Double getPrecio() {
+		return precio;
+	}
+
+	public void setPrecio(Double precio) {
+		this.precio = precio;
+	}
+
+	public Integer getStock() {
+		return stock;
+	}
+
+	public void setStock(Integer stock) {
+		this.stock = stock;
+	}
+
+	public Boolean getActivo() {
+		return activo;
+	}
+
+	public void setActivo(Boolean activo) {
+		this.activo = activo;
+	}
+	
+	
+	
+}
---
+++ src/main/java/com/example/demo/repository/ProductoRepository.java
@@ -0,0 +1,12 @@
+package com.example.demo.repository;
+
+import com.example.demo.model.Producto;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+
+
+@Repository
+public interface ProductoRepository extends JpaRepository<Producto,Long>{
+	
+}
